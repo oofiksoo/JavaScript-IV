935c0344ef0143882ab8cf0d57f8f99bc34312d9
Joseph Graham <Joseph@Chasingthefiks.net>
2019-10-17 21:55:33 -0600

    MVP Met All Requirements

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -56,69 +56,101 @@ class Instructor extends Person {
     }
 }
 const fred = new Instructor({
-        name: 'Fred',
-        location: 'Bedrock',
-        age: 37,
-        favLanguage: 'JavaScript',
-        specialty: 'Front-end',
-        catchPhrase: `Don't forget the homies`
-    })
-    /*
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+})
 
-    #### Student
+/*
 
-    * Now we need some students!
-    * Student uses the same attributes that have been set up by Person
-    * Student has the following unique props:
-      * `previousBackground` i.e. what the Student used to do before Lambda School
-      * `className` i.e. CS132
-      * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
-    * Student has the following methods:
-      * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
-      * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
-      * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
+#### Student
 
-    */
+* Now we need some students!
+* Student uses the same attributes that have been set up by Person
+* Student has the following unique props:
+  * `previousBackground` i.e. what the Student used to do before Lambda School
+  * `className` i.e. CS132
+  * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
+* Student has the following methods:
+  * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
+  * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
+  * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
+
+*/
 class Student extends Person {
     constructor(sAttr) {
         super(sAttr);
         this.previousBackground = sAttr.previousBackground,
             this.className = sAttr.className,
-            this.favSubjects = sAttr.favSubjects
+            this.favSubjects = [sAttr.favSubjects]
     }
     listSubjects() {
-
+        this.favSubjects.forEach(element => {
+            console.log(`${this.favSubjects}`);
+        });
     }
-    PRAssignment() {
-
+    PRAssignment(sub) {
+        console.log(`${this.name} has submitted a PR for ${sub}`);
     }
-    sprintChallenge() {
-
+    sprintChallenge(sub) {
+        console.log(`${this.name} has begun sprint challenge on ${sub}`);
     }
 }
-/*
+const Paul = new Student({
+    name: 'Paul',
+    location: 'Bedrock',
+    age: 37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`,
+    className: 'biology',
+    favSubjects: ['math', 'science', 'keybording']
+})
+fred.demo(".Net ADAL API Managment");
+fred.grade(Paul, "fishing");
+Paul.listSubjects();
+Paul.PRAssignment("Computers");
+Paul.sprintChallenge("User Interface")
+    /*
 
-#### Project Manager
+    #### Project Manager
 
-* Now that we have instructors and students, we'd be nowhere without our PM's
-* ProjectManagers are extensions of Instructors
-* ProjectManagers have the following unique props:
-  * `gradClassName`: i.e. CS1
-  * `favInstructor`: i.e. Sean
-* ProjectManagers have the following Methods:
-  * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
-  * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`*
-*/
+    * Now that we have instructors and students, we'd be nowhere without our PM's
+    * ProjectManagers are extensions of Instructors
+    * ProjectManagers have the following unique props:
+      * `gradClassName`: i.e. CS1
+      * `favInstructor`: i.e. Sean
+    * ProjectManagers have the following Methods:
+      * `standUp` a method that takes in a slack channel and logs `{name} announces to {channel}, @channel standy times!​​​​​
+      * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`*
+    */
 class ProjectManager extends Instructor {
     constructor(pmAttr) {
         super(pmAttr);
         this.gradClassName = pmAttr.gradClassName,
             this.favInstructor = pmAttr.favInstructor
     }
-    standUp() {
-
+    standUp(chan) {
+        console.log(`${this.name} announces to ${chan}, @channel standy times!​​​​​`);
     }
-    debugsCode() {
-
+    debugsCode(sAttr, sub) {
+        console.log(`${this.name} debugs ${sAttr.name}'s code on ${sub}`);
     }
 }
+const John = new ProjectManager({
+    name: 'John',
+    location: 'Bedrock',
+    age: 37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`,
+    className: 'biology',
+    favSubjects: ['math', 'science', 'keybording'],
+    gradClassName: 'WEB25',
+    favInstructor: 'Joseph'
+})
+John.standUp("The Big Channel");
+John.debugsCode(Paul, "Data 101");
